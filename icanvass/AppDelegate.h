3e645c3574198e3a4c259830398996397e9717d2
Roman Kot <romek@oke.pl>
2014-03-08 20:15:13 +0100

    Initial Commit

---
+++ icanvass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  icanvass
+//
+//  Created by Roman Kot on 08.03.2014.
+//  Copyright (c) 2014 Roman Kot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ icanvass/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  icanvass
+//
+//  Created by Roman Kot on 08.03.2014.
+//  Copyright (c) 2014 Roman Kot. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
