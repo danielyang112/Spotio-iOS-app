847515fe4ad9241299ba30afb0bb95b528eb59cf
Roman Kot <romek@oke.pl>
2014-08-11 17:09:29 +0200

    services.spotio i.o. app.spotio

--- icanvass/ICRequestManager.h
+++ icanvass/ICRequestManager.h
@@ -8,8 +8,8 @@
 
 #define kUserNameKey @"UserName"
 #define kCompanyNameKey @"CompanyName"
-//#define kBaseUrl @"http://app.spotio.com:888/"
-#define kBaseUrl @"http://app.spotio.com:8080/"
+#define kBaseUrl @"http://services.spotio.com:888/"
+//#define kBaseUrl @"http://services.spotio.com:8080/"
 #define kPasswordKey @"Password"
 #define kRefreshDate @"refreshdate"
 
